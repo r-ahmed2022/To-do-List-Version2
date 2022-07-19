96c92a3a9b879392079eb4731fa50180caae69b5
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-19 15:42:47 +0530

    updated index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -6,16 +7,17 @@ module.exports = {
     index: './src/index.js'
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
-  output: {
+   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ],
   
    devServer: {
     static: './dist',
